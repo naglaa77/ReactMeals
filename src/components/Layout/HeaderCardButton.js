5997da451b60c7a9f271c38150e142b30a6f77bc
naglaa77 <sama_hap@yahoo.com>
2022-12-12 19:51:47 +0100

    use state for showing and hide the cart with dummy data

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -3,7 +3,7 @@ import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCardButton.module.css";
 const HeaderCardButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
